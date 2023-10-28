171a26ad045e086656291a7fb397abe71268e2aa
savagegraphics <olawaleraji188@gmail.com>
2023-10-28 12:05:23 -0700

    first commit

--- src/components/Props.tsx
+++ src/components/Props.tsx
@@ -1,55 +1,55 @@
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
 
-const Header = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false)
+// const Header = () => {
+//   const [isMenuOpen, setIsMenuOpen] = useState(false)
+//   const [isDropdownOpen, setIsDropdownOpen] = useState(false)
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen)
-  }
+//   const toggleMenu = () => {
+//     setIsMenuOpen(!isMenuOpen)
+//   }
 
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen)
-  }
+//   const toggleDropdown = () => {
+//     setIsDropdownOpen(!isDropdownOpen)
+//   }
 
-  return (
-    <header className='flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full bg-white text-sm py-3 md:py-0 dark:bg-gray-800'>
-      <nav
-        className='max-w-[85rem] w-full mx-auto px-4 md:px-6 lg:px-8'
-        aria-label='Global'
-      >
-        <div className='relative md:flex md:items-center md:justify-between'>
-          <div className='flex items-center justify-between'>
-            <a
-              className='flex-none text-xl font-semibold dark:text-white'
-              href='#'
-              aria-label='Brand'
-            >
-              Brand
-            </a>
-            <div className='md:hidden'>
-              <button
-                type='button'
-                className='hs-collapse-toggle p-2 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800'
-                onClick={toggleMenu}
-                aria-label='Toggle navigation'
-              >
-                {/* Add your toggle icon SVG code here */}
-              </button>
-            </div>
-          </div>
+//   return (
+//     <header className='flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full bg-white text-sm py-3 md:py-0 dark:bg-gray-800'>
+//       <nav
+//         className='max-w-[85rem] w-full mx-auto px-4 md:px-6 lg:px-8'
+//         aria-label='Global'
+//       >
+//         <div className='relative md:flex md:items-center md:justify-between'>
+//           <div className='flex items-center justify-between'>
+//             <a
+//               className='flex-none text-xl font-semibold dark:text-white'
+//               href='#'
+//               aria-label='Brand'
+//             >
+//               Brand
+//             </a>
+//             <div className='md:hidden'>
+//               <button
+//                 type='button'
+//                 className='hs-collapse-toggle p-2 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800'
+//                 onClick={toggleMenu}
+//                 aria-label='Toggle navigation'
+//               >
+//                 {/* Add your toggle icon SVG code here */}
+//               </button>
+//             </div>
+//           </div>
 
-          <div
-            id='navbar-collapse-with-animation'
-            className={`hs-collapse ${
-              isMenuOpen ? 'block' : 'hidden'
-            } overflow-hidden transition-all duration-300 basis-full grow md:block`}
-          >
-            {/* Your menu items here */}
-          </div>
-        </div>
-      </nav>
-    </header>
-  )
-}
-export default Header
+//           <div
+//             id='navbar-collapse-with-animation'
+//             className={`hs-collapse ${
+//               isMenuOpen ? 'block' : 'hidden'
+//             } overflow-hidden transition-all duration-300 basis-full grow md:block`}
+//           >
+//             {/* Your menu items here */}
+//           </div>
+//         </div>
+//       </nav>
+//     </header>
+//   )
+// }
+// export default Header
